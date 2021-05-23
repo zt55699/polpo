9ec3939b3717c130ce8e592e185365bd006e06fe
Bernice Lam <bernicelam@Bernices-MBP.hitronhub.home>
2021-05-22 17:28:35 -0700

    banner added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src="./pictures/banner.jpg" alt="banner" className="banner"></img>
         <img src="./pictures/polpo.JPG" alt="logo" className="polpo-logo"></img>
         <p>
           Edit <code>src/App.js</code> and save to reload.
