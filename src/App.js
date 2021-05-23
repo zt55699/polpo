7bccca9f07c5048e7faba8692bb2cc8499914238
Bernice Lam <bernicelam@Bernices-MBP.hitronhub.home>
2021-05-22 23:53:50 -0700

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import{BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Router>
+      <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src="./pictures/banner.jpg" alt="banner" className="banner"></img>
         <img src="./pictures/polpo.JPG" alt="logo" className="polpo-logo"></img>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+</>
+    
   );
 }
 
